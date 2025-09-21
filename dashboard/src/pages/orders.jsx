01526b576935e9cedeb851992e97c541c22a4a65
sgn <newaresuyash7583@gmail.com>
2025-09-21 21:47:26 +0530

    jsx

--- dashboard/src/pages/orders.jsx
+++ dashboard/src/pages/orders.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Sidebar from "../components/Sidebar.jsx";
-import TopNav from "../components/TopNa.jsx";
+import TopNav from "../components/TopNav.jsx";
 import OrdersTable from "./OrdersTable.jsx";
 import "../styles/OrdersPage.css";
 
