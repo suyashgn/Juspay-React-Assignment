6fc5e2d5773af627b21d79fbe41faa7d2aa3adb0
sgn <newaresuyash7583@gmail.com>
2025-09-21 15:04:36 +0530

    deplyment issue

--- dashboard/vite.config.js
+++ dashboard/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
- base: "/Juspay-React-Assignment/"   
+//base: "/Juspay-React-Assignment/"  
 })
